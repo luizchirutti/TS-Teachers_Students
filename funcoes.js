b4692a496317512684745616c9b3b708cea2e078
Luiz Carlos Chirutti <37088740+luizchirutti@users.noreply.github.com>
2019-03-28 18:13:13 -0300

    Front-end
    HTML,JS

---
+++ funcoes.js
@@ -0,0 +1,25 @@
+
+function redirect() {
+
+			window.location.href = "logado.html"
+}
+
+function Function(id) {
+	var x = document.getElementById(id);
+	if (x.className.indexOf("w3-show") == -1) {
+	  x.className += " w3-show";
+	  x.previousElementSibling.className += " w3-theme-d1";
+	} else { 
+	  x.className = x.className.replace("w3-show", "");
+	  x.previousElementSibling.className = 
+	  x.previousElementSibling.className.replace(" w3-theme-d1", "");
+	}
+  }
+  function openNav() {
+	var x = document.getElementById("navDemo");
+	if (x.className.indexOf("w3-show") == -1) {
+	  x.className += " w3-show";
+	} else { 
+	  x.className = x.className.replace(" w3-show", "");
+	}
+  }
